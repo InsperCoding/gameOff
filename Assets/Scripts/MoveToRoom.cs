e25939857b1d96929ad7b29f85f96443f252d154
HenriqueRBomfim <henriquerb1@al.insper.edu.br>
2022-11-20 17:04:34 -0300

    Agora tem 20% de uma batalha no estilo Pokémon iniciar quando o jogador passar por portas. Elementos das salas do Vini não estão funcionando

--- Assets/Scripts/MoveToRoom.cs
+++ Assets/Scripts/MoveToRoom.cs
@@ -6,10 +6,14 @@ using UnityEngine.SceneManagement;
 public class MoveToRoom : MonoBehaviour
 {
     public int RoomNumber;
-
     public void MoveNextRoom()
     {
-        SceneManager.LoadScene(RoomNumber);
+        if(Random.Range(0,100) >= 80)
+        {
+            SceneManager.LoadScene(14);
+        } else
+        {
+            SceneManager.LoadScene(RoomNumber);
+        }
     }
-
 }

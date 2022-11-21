a10b1944cf160f60223e4c55cc6697c37cc6f5cf
joaogcfa <joaogcfa@al.insper.edu.br>
2022-11-20 21:14:14 -0300

    animacoes de powerup

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -10,12 +10,17 @@ public class PowerUp : MonoBehaviour
     private CircleCollider2D cc;
     public float starterRadious = 1.5f;
     public float finalRadious = 0.3f;
-    private KeyCode[] keys = { KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4 };  // 1 = Air, 2 = Fire, 3 = Water, 4 = Earth
+    private KeyCode[] keys = { KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4 };  // 1 = Eletrical, 2 = Fire, 3 = Water, 4 = Earth
     private string key1, key2;
+
+    private SpriteRenderer sr;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         cc = GetComponent<CircleCollider2D>();
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,116 +32,62 @@ public class PowerUp : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.Alpha1) || Input.GetKey(KeyCode.Alpha2) || Input.GetKey(KeyCode.Alpha3) || Input.GetKey(KeyCode.Alpha4))
             {
-                for (int i = 0; i < keys.Length; i++)
-                {
-                    if (Input.GetKey(keys[i]))
-                    {
-                        key1 = keys[i].ToString();
-                    }
-                }
-                // key1 = (Input.GetKey(KeyCode.Alpha1) || Input.GetKey(KeyCode.Alpha2) || Input.GetKey(KeyCode.Alpha3) || Input.GetKey(KeyCode.Alpha4)).ToString();
-                if (Input.GetKeyUp(KeyCode.Alpha1) || Input.GetKeyUp(KeyCode.Alpha2) || Input.GetKeyUp(KeyCode.Alpha3) || Input.GetKeyUp(KeyCode.Alpha4))
-                {
-                    for (int i = 0; i < keys.Length; i++)
-                    {
-                        if (Input.GetKeyUp(keys[i]))
-                        {
-                            key2 = keys[i].ToString();
-                        }
-                    }
-                    print("Power Up Activated");
-                    ActivatePowerUp(key1, key2);
-                    coolDown = false;
-                    StartCoroutine("PowerUpCoolDown");
-                }
+                print(Input.inputString);
+                key1 = Input.inputString;
+                print("Power Up Activated");
+                ActivatePowerUp(key1);
+                coolDown = false;
+                StartCoroutine("PowerUpCoolDown");
             }
         }
-
-
     }
 
 
-    void ActivatePowerUp(string key1, string key2)
+    void ActivatePowerUp(string key1)
     {
 
 
-        if (key1 == "Alpha1")
+        if (key1 == "1")
         {
-            if (key2 == "Alpha2")
+            if (GlobalMagicSlots.eletricLevel > 0)
             {
-                print("Air + Fire");
-                cc.radius = finalRadious;
-            }
-            else if (key2 == "Alpha3")
-            {
-                print("Air + Water");
-                cc.radius = finalRadious;
-            }
-            else if (key2 == "Alpha4")
-            {
-                print("Air + Earth");
+                print("Eletrical");
+                animator.SetInteger("Pup", 1);
                 cc.radius = finalRadious;
             }
         }
-        else if (key1 == "Alpha2")
+        else if (key1 == "2")
         {
-            if (key2 == "Alpha1")
+            if (GlobalMagicSlots.fireLevel > 0)
             {
-                print("Fire + Air");
-                cc.radius = finalRadious;
-            }
-            else if (key2 == "Alpha3")
-            {
-                print("Fire + Water");
-                cc.radius = finalRadious;
-            }
-            else if (key2 == "Alpha4")
-            {
-                print("Fire + Earth");
+                print("Fire");
+                animator.SetInteger("Pup", 2);
                 cc.radius = finalRadious;
             }
         }
-        else if (key1 == "Alpha3")
+        else if (key1 == "3")
         {
-            if (key2 == "Alpha1")
+            if (GlobalMagicSlots.waterLevel > 0)
             {
-                print("Water + Air");
-                cc.radius = finalRadious;
-            }
-            else if (key2 == "Alpha2")
-            {
-                print("Water + Fire");
-                cc.radius = finalRadious;
-            }
-            else if (key2 == "Alpha4")
-            {
-                print("Water + Earth");
+                print("Water");
+                animator.SetInteger("Pup", 3);
                 cc.radius = finalRadious;
             }
         }
-        else if (key1 == "Alpha4")
+        else if (key1 == "4")
         {
-            if (key2 == "Alpha1")
+            if (GlobalMagicSlots.earthLevel > 0)
             {
-                print("Earth + Air");
-                cc.radius = finalRadious;
-            }
-            else if (key2 == "Alpha2")
-            {
-                print("Earth + Fire");
-                cc.radius = finalRadious;
-            }
-            else if (key2 == "Alpha3")
-            {
-                print("Earth + Water");
+                print("Earth");
+                animator.SetInteger("Pup", 4);
                 cc.radius = finalRadious;
             }
         }
 
-
-        cc.radius = starterRadious;
-        print("cu");
-        InvokeRepeating("Shrink", 0.1f, 0.1f);
+        if (cc.radius > starterRadious)
+        {
+            InvokeRepeating("Shrink", 0.1f, 0.1f);
+        }
     }
     IEnumerator PowerUpCoolDown()
     {
@@ -147,14 +98,15 @@ public class PowerUp : MonoBehaviour
     void Shrink()
     {
 
-        if (cc.radius > 0.3f)
+        if (cc.radius > starterRadious)
         {
             cc.radius -= 0.1f;
         }
         else
         {
             print("aaaa");
-            cc.radius = finalRadious;
+            cc.radius = starterRadious;
+            animator.SetInteger("Pup", 0);
             CancelInvoke("Shrink");
         }
     }

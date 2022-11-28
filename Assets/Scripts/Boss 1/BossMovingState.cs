842126ad191e5ff4f453585039a3e8a8eaea1f7a
RafaelMALima <rafaelmal@al.insper.edu.br>
2022-11-28 10:22:17 -0300

    Animações adicionadas no projeto

--- Assets/Scripts/Boss 1/BossMovingState.cs
+++ Assets/Scripts/Boss 1/BossMovingState.cs
@@ -11,21 +11,29 @@ public class BossMovingState : BossBaseState
         rand = new System.Random();
     }
 
-    public override void UpdateState(BossStateManager boss){
-        Vector2 distance = boss.player.position - boss.gameObject.transform.position;
+    public override void UpdateState(BossStateManager Boss){
+        Vector2 distance = Boss.player.position - Boss.gameObject.transform.position;
         Vector2 direction = new Vector2(distance.x, distance.y);
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        boss.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+        Boss.transform.GetChild(0).gameObject.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        if (distance.x > 0)
+        {
+            Boss.GetComponent<SpriteRenderer>().flipX = true;
+        } else
+        {
+            Boss.GetComponent<SpriteRenderer>().flipX = false;
+        }
 
         if (Math.Sqrt(distance.x*distance.x + distance.y*distance.y) > 2){
-            boss.rb.velocity = (direction * boss.speed);
+            Boss.rb.velocity = (direction * Boss.speed);
         } else {
             int isHorizontal = rand.Next(0,2);
             if (isHorizontal == 1){
-                boss.SwitchState(boss.HorizontalAttackState);
+                Boss.SwitchState(Boss.HorizontalAttackState);
             } else {
-                boss.SwitchState(boss.VerticalAttackState);
+                Boss.SwitchState(Boss.VerticalAttackState);
             }
         }
     }

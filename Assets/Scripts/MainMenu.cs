adc4f3dd0d9fd31cce8ad8feace0f56a42646966
HenriqueRBomfim <henriquerb1@al.insper.edu.br>
2022-11-28 15:03:22 -0300

    Arrumei o Canvas de todas as salas e fiz ajustes nas câmeras

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("RoomStart");
+        SceneManager.LoadScene("Room0");
     }
 
     public void QuitGame()

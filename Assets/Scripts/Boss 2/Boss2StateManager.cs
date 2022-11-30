40cc8c8983f4fe5c5db2295ea08b30d55330ea3f
RafaelMALima <rafaelmal@al.insper.edu.br>
2022-11-30 16:39:20 -0300

    O boss 2 agora morre toma dano

--- Assets/Scripts/Boss 2/Boss2StateManager.cs
+++ Assets/Scripts/Boss 2/Boss2StateManager.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Boss2StateManager : MonoBehaviour
 {
     Boss2BaseState currentState;
+    float timeSinceLastHit;
+    int currentHealth;
+
+    public int maxHealth;
     public Boss2CloseRangeAttack CloseRangeAttack = new Boss2CloseRangeAttack();
     public Boss2FireAttackState FireAttackState = new Boss2FireAttackState();
     public Boss2MovementState MovingState = new Boss2MovementState();
@@ -16,6 +20,7 @@ public class Boss2StateManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        currentHealth = maxHealth;
         rb = GetComponent<Rigidbody2D>();
         currentState = MovingState;
 
@@ -25,10 +30,21 @@ public class Boss2StateManager : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        timeSinceLastHit += Time.deltaTime;
         currentState.UpdateState(this);
+        if (currentHealth <= 0 ) { Destroy(gameObject); }
     }
     public void SwitchState(Boss2BaseState state){
         currentState = state;
         currentState.EnterState(this);
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Bateu");
+        if ((collider.gameObject.tag == "Sword" || collider.gameObject.tag == "PowerUp") && timeSinceLastHit > 1)
+        {
+            currentHealth -= 1;
+            timeSinceLastHit = 0;
+        }
     }
 }

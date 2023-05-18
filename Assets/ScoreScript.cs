65d713c2db80852fa73ab0cc6cd8a6a53e5e07e0
Bruno Rodrigues <bf17669@gmail.com>
2023-05-18 09:50:43 -0300

    Score

--- Assets/ScoreScript.cs
+++ Assets/ScoreScript.cs
@@ -6,25 +6,32 @@ using System;
 
 public class ScoreScript : MonoBehaviour
 {
-    public int score;
     public TMP_Text text;
+
     // Start is called before the first frame update
     void Start()
     {
-        score = GameObject.Find("Player").GetComponent<PlayerHealth>().score;
+        // Obtém o score atualizado da classe ScoreManager
+        int score = ScoreManager.score;
+        UpdateScoreText(score);
     }
 
     // Update is called once per frame
     void Update()
     {
-        text.text = String.Format("Score:{0}",score);
-        GameObject.Find("Player").GetComponent<PlayerHealth>().score = score;
-        //score++;
+        // Atualiza o texto do score a partir do valor atualizado em ScoreManager
+        UpdateScoreText(ScoreManager.score);
     }
 
     public void AddScore(int pts)
     {
-        Debug.Log("PINTO");
-        score += pts;
+        // Adiciona pontos ao score
+        ScoreManager.score += pts;
+    }
+
+    private void UpdateScoreText(int score)
+    {
+        // Atualiza o texto com o valor do score
+        text.text = String.Format("Score: {0}", score);
     }
 }
